27092782c23e05ca5b444403cb8ebfcfea1c19d1
JonStevet <JonSteve@outlook.com>
2023-03-27 17:25:29 -0500

    implements cors whitelist

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
+const boom = require('@hapi/boom');
 
 const { errorLogger, errorServer, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(express.json());
+
+// CORS, domain access control allow whitelist
+const whitelist = [
+  undefined, //acept same origin
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.some((item) => item == origin)) {
+      callback(null, true);
+    } else {
+      callback(boom.unauthorized(`Domain: ${origin}, is not allowed.`));
+    }
+  },
+};
+
+app.use( cors(corsOptions) );
 
 //Home end point
 app.get('/', (req, res) => {
-  res.send('Hola, este es mi backend server');
+  res.send('Backend server');
 });
 
 routerApi(app);
@@ -17,7 +38,6 @@ routerApi(app);
 app.use(errorLogger);
 app.use(boomErrorHandler);
 app.use(errorServer);
-
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -2,7 +2,6 @@ const boom = require('@hapi/boom');
 
 function validator(schema, property){
     return (req, res, next) => {
-        console.log(req[property])
         const result = schema.validate(req[property]);
         result.error ? next(boom.badRequest(result.error)) : next();
     }
